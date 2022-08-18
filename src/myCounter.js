c730b9aa1477f453920cdf17cd6a759711816570
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-18 15:39:50 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,18 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let contador = 2; contador <= 3; contador += 1) {
+      myArray.push(contador);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
+// Como as variaveis estavam utilizando "var" o escopo estava vazando.
+// Dessa forma, atualizei para "const" e "let", e também criei uma nova "let"
 
 module.exports = myCounter;
